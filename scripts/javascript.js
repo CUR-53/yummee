e35bd042ab7faf7966ca1cf09aa743911007a36c
CUR53 <114309130+CUR-53@users.noreply.github.com>
2023-02-02 21:09:15 +0100

    update

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,32 @@
+/* filter function */
+const categoryTitle = document.querySelectorAll('.category-title');
+const allCategoryPosts = document.querySelectorAll('.all');
+
+for (let i = 0; i < categoryTitle.length; i++) {
+  categoryTitle[i].addEventListener(
+    'click',
+    filterPosts.bind(this, categoryTitle[i])
+  );
+}
+
+function filterPosts(item) {
+  changeActivePosition(item);
+  for (let i = 0; i < allCategoryPosts.length; i++) {
+    if (allCategoryPosts[i].classList.contains(item.attributes.id.value)) {
+      allCategoryPosts[i].style.display = 'block';
+    } else {
+      allCategoryPosts[i].style.display = 'none';
+    }
+  }
+}
+
+function changeActivePosition(activeItem) {
+  for (let i = 0; i < categoryTitle.length; i++) {
+    categoryTitle[i].classList.remove('active');
+  }
+  activeItem.classList.add('active');
+}
+
 // Burgermenu
 const burgermenu = document.querySelector('.burgermenu_icon');
 const navMenu = document.querySelector('.burgermenu_container');
