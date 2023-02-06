a7e097c77d0cc83157699fc6f68a414c6868c9da
CUR53 <114309130+Matias1706@users.noreply.github.com>
2023-02-06 09:51:50 +0100

    update

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -33,6 +33,8 @@ let recipeData = [
     time: '45',
     person: '4',
     language: 'Mexicansk',
+
+    onClick: 'onclick="location.href=\'recipes/Amerikanske_pandekager.html\';"',
   },
 
   {
@@ -70,6 +72,105 @@ let recipeData = [
     time: '25',
     person: '4',
     language: 'Fransk',
+
+    onClick: 'onclick="location.href=\'recipes/Amerikanske_pandekager.html\';"',
+  },
+
+  {
+    type: 'dinner',
+    category: '/assets/dinner_icon.svg',
+    categoryAltText: 'dinner ikon',
+
+    image: '/assets/wok-med-kylling-nudler-og-grontsager.jpg',
+    imageAltText: 'Wok med kylling, nudler og grøntsager',
+
+    title: 'Wok med kylling, nudler og grøntsager',
+    description:
+      'Wok med kylling, nudler og grøntsager - en dejlig asiatisk inspireret wokret med masser af grønt og sprød topping af ristede cashewnødder og friske forårsløg. ',
+
+    time: '30',
+    person: '4',
+    language: 'Japansk',
+
+    onClick:
+      'onclick="location.href=\'recipes/Tomatsuppe_med_sprød_gremolata.html\';"',
+  },
+
+  {
+    type: 'lunch',
+    category: '/assets/lunch_icon.svg',
+    categoryAltText: 'lunch ikon',
+
+    image: '/assets/mozzarella-sticks.jpg',
+    imageAltText: 'Mozzarella sticks',
+
+    title: 'Mozzarella sticks',
+    description:
+      'Cheesesticks eller mozzarella sticks er fastfood eller street food, når det er rigtig fedt eller fedtet... Her bliver mozarella sticks serveret i en ny udgave med mere smag.',
+
+    time: '30',
+    person: '3',
+    language: 'Fransk',
+
+    onClick: 'onclick="location.href=\'recipes/Amerikanske_pandekager.html\';"',
+  },
+
+  {
+    type: 'breakfast',
+    category: '/assets/breakfast_icon.svg',
+    categoryAltText: 'breakfast ikon',
+
+    image: '/assets/belgiske-vafler.jpg',
+    imageAltText: 'Belgiske vafler',
+
+    title: 'Belgiske vafler',
+    description:
+      'Belgiske vafler er en rigtig dejlig hyggespise. Server hjemmelavede belgiske vafler efter en kølig gåtur, til weekendens morgenmad eller til brunch. Vaflerne er sprøde udenpå og bløde indeni.',
+
+    time: '30',
+    person: '3',
+    language: 'Belgiske',
+
+    onClick: 'onclick="location.href=\'recipes/Amerikanske_pandekager.html\';"',
+  },
+
+  {
+    type: 'dessert',
+    category: '/assets/dessert_icon.svg',
+    categoryAltText: 'dessert ikon',
+
+    image: '/assets/muffins-med-chokoladestykker.jpg',
+    imageAltText: 'Muffins med chokoladestykker',
+
+    title: 'Muffins med chokoladestykker',
+    description:
+      'Muffins er betegnelsen for en meget populær kage, som er bagt i en lille, rund portionsform - og der findes et utal af varianter.',
+
+    time: '45',
+    person: '3',
+    language: 'Amerikansk',
+
+    onClick: 'onclick="location.href=\'recipes/Amerikanske_pandekager.html\';"',
+  },
+
+  {
+    type: 'dinner',
+    category: '/assets/dinner_icon.svg',
+    categoryAltText: 'dinner ikon',
+
+    image: '/assets/spaghetti-carbonara.jpg',
+    imageAltText: 'Spaghetti Carbonara',
+
+    title: 'Spaghetti Carbonara',
+    description:
+      'Spaghetti Carbonara - næsten som at sidde på en italiensk restaurant. Vi foreslår en tomatsalat til Spaghetti Cabonara.',
+
+    time: '45',
+    person: '4',
+    language: 'Italiensk ',
+
+    onClick:
+      'onclick="location.href=\'recipes/Tomatsuppe_med_sprød_gremolata.html\';"',
   },
 ];
 
