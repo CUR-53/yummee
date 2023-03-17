55ec2d2803828b0fa49c000d0dea59447ff4d792
CUR53 <114309130+CUR-53@users.noreply.github.com>
2023-03-17 16:39:32 +0100

    update

--- scripts/recipeDetailTmpl.js
+++ scripts/recipeDetailTmpl.js
@@ -3,33 +3,30 @@ export const renderDetailRecipe = (data) => {
   let ingredientsListTwoHTML = '';
   let howToHTML = '';
 
-  if (Array.isArray(data)) {
-    data.forEach((recipeData) => {
-      recipeData.ingredients[0].list.forEach((ingredient) => {
-        ingredientsListHTML += `
-          <li>
-            <p>${ingredient.name}</p>
-            <p>${ingredient.amount} ${ingredient.type}</p>
-          </li>
-        `;
-      });
-
-      recipeData.ingredients[1].listTwo.forEach((ingredient) => {
-        ingredientsListTwoHTML += `
-          <li>
-            <p>${ingredient.name}</p>
-            <p>${ingredient.amount} ${ingredient.type}</p>
-          </li>
-        `;
-      });
-
-      recipeData.howToList.forEach((step) => {
-        howToHTML += `
-          <li>${step.howTo}</li>
-        `;
-      });
+  if (typeof data === 'object') {
+    data.ingredients[0].list.forEach((ingredient) => {
+      ingredientsListHTML += `
+        <li>
+          <p>${ingredient.name}</p>
+          <p>${ingredient.amount} ${ingredient.type}</p>
+        </li>
+      `;
     });
-  } else {
+
+    data.ingredients[1].listTwo.forEach((ingredient) => {
+      ingredientsListTwoHTML += `
+        <li>
+          <p>${ingredient.name}</p>
+          <p>${ingredient.amount} ${ingredient.type}</p>
+        </li>
+      `;
+    });
+
+    data.howToList.forEach((step) => {
+      howToHTML += `
+        <li>${step.howTo}</li>
+      `;
+    });
   }
 
   return `
@@ -59,9 +56,9 @@ export const renderDetailRecipe = (data) => {
             ${data.person} personer
           </h3>
           <ul>
-            <h4>${data.title}</h4>
+            <h4>${data.listName}</h4>
             ${ingredientsListHTML}
-            <h4>Tilbehør</h4>
+            <h4>${data.listNameTwo}</h4>
             ${ingredientsListTwoHTML}
           </ul>
         </div>
@@ -79,57 +76,3 @@ export const renderDetailRecipe = (data) => {
     </div>
   `;
 };
-
-/* let recipeDetails = document.querySelector('.recipe_details');
-  recipeDetails.innerHTML = ''; */
-
-/* 
-    recipeDetails.innerHTML += `
-      <div class="grid_recipe_container">
-        <div class="recipe_mainText_box">
-          <h1>${accord.title}</h1>
-          <div class="mainText_info_boxes">
-            <p><img src="/assets/clock_icon.svg" alt="tid ikon" />${accord.time}m</p>
-            <p><img src="/assets/per_icon.svg" alt="person ikon" />${accord.person} per.</p>
-            <p>
-              <img src="/assets/tool_icon.svg" alt="køkken ikon" />${accord.language}
-            </p>
-          </div>
-          <span>
-            ${accord.description}
-          </span>
-        </div>
-        <div
-          class="recipe_img_box"
-          style="background-image: url(${accord.image})"
-        ></div>
-        <div class="recipe_ingredients_box">
-          <div class="ingredients_list">
-            <h2>Ingredienser</h2>
-            <h3>
-              <img src="/assets/per_icon_white.svg" alt="person ikon" />
-              ${accord.person} personer
-            </h3>
-            <ul>
-              <h4>${accord.title}</h4>
-              ${ingredientsListHTML}
-              <h4>Tilbehør</h4>
-              ${ingredientsListTwoHTML}
-            </ul>
-          </div>
-        </div>
-        <div class="recipe_howTo_box">
-          <div class="howTo_list">
-            <h2>Sådan gør du</h2>
-            <ol>
-              <h4>${accord.title}</h4>
-              ${howToHTML}
-              <h4>Velbekomme!</h4>
-            </ol>
-          </div>
-        </div>
-      </div>
-    `;
-  }); */
-
-/* renderDetailRecipe(recipeData); */
--- scripts/recipes.js
+++ scripts/recipes.js
@@ -1,260 +1,302 @@
-  const recipeData = [
-    {
-      id: 0,
+const recipeData = [
+  {
+    id: 0,
 
-      type: 'breakfast',
-      category: '/assets/breakfast_icon.svg',
-      categoryAltText: 'breakfast ikon',
+    type: 'breakfast',
+    category: '/assets/breakfast_icon.svg',
+    categoryAltText: 'breakfast ikon',
 
-      image: '/assets/amerikanske-pandekager.jpg',
-      imageAltText: 'Amerikanske pandekager',
+    image: '/assets/amerikanske-pandekager.jpg',
+    imageAltText: 'Amerikanske pandekager',
 
-      title: 'Amerikanske pandekager',
-      shortDescription:
-        'Amerikanske pandekager passer perfekt til både brunch og dessert. Bag de små, tykke amerikanske pandekager i en almindelig pande eller i en blinispande og servér med blåbær.',
-      description:
-        'Amerikanske pandekager passer perfekt til både brunch og dessert. Bag de små, tykke amerikanske pandekager i en almindelig pande eller i en blinispande og servér med blåbær, ahornsirup og smør. Det er både nemt og dejligt at hygge sig med at lave amerikanske pandekager. Og det går rigtig hurtigt med at få de amerikanske pandekager spist!',
+    title: 'Amerikanske pandekager',
+    shortDescription:
+      'Amerikanske pandekager passer perfekt til både brunch og dessert. Bag de små, tykke amerikanske pandekager i en almindelig pande eller i en blinispande og servér med blåbær.',
+    description:
+      'Amerikanske pandekager passer perfekt til både brunch og dessert. Bag de små, tykke amerikanske pandekager i en almindelig pande eller i en blinispande og servér med blåbær, ahornsirup og smør. Det er både nemt og dejligt at hygge sig med at lave amerikanske pandekager. Og det går rigtig hurtigt med at få de amerikanske pandekager spist!',
 
-      time: '30',
-      person: '4',
-      language: 'Amerikansk',
+    time: '30',
+    person: '4',
+    language: 'Amerikansk',
 
-      ingredients: [
-        {
-          list: [
-            { name: 'Hvedemel', amount: '300', type: 'g' },
-            { name: 'Mælk', amount: '3', type: 'dl' },
-            { name: 'Skyr eller græsk yoghurt', amount: '2', type: 'dl' },
-            { name: 'Æg', amount: '2', type: 'stk' },
-            { name: 'Sukker', amount: '2', type: 'spsk' },
-            { name: 'Groft salt', amount: '¼', type: 'tsk' },
-            { name: 'Bagepulver', amount: '1½', type: 'tsk' },
-            { name: 'Smør - smeltet', amount: '50', type: 'g' },
-          ],
-        },
-        {
-          listTwo: [
-            { name: 'Ahornsirup', amount: '1', type: 'dl' },
-            { name: 'Blåbær', amount: '100', type: 'g' },
-            { name: 'Smør', amount: '50', type: 'g' },
-          ],
-        },
-      ],
+    ingredients: [
+      {
+        list: [
+          { name: 'Hvedemel', amount: '300', type: 'g' },
+          { name: 'Mælk', amount: '3', type: 'dl' },
+          { name: 'Skyr eller græsk yoghurt', amount: '2', type: 'dl' },
+          { name: 'Æg', amount: '2', type: 'stk' },
+          { name: 'Sukker', amount: '2', type: 'spsk' },
+          { name: 'Groft salt', amount: '¼', type: 'tsk' },
+          { name: 'Bagepulver', amount: '1½', type: 'tsk' },
+          { name: 'Smør - smeltet', amount: '50', type: 'g' },
+        ],
+      },
+      {
+        listTwo: [
+          { name: 'Ahornsirup', amount: '1', type: 'dl' },
+          { name: 'Blåbær', amount: '100', type: 'g' },
+          { name: 'Smør', amount: '50', type: 'g' },
+        ],
+      },
+    ],
 
-      howToList: [
-        { howTo: 'Pisk mel og mælk sammen med en håndmixer.' },
-        { howTo: 'Tilsæt kvark og pisk æg og resten af ingredienserne i.' },
-        {
-          howTo:
-            'Fordel 4 portioner dej a ca. ½ dl på panden. Bag pandekagerne ca. 2 min. på hver side. ',
-        },
-        { howTo: 'Server pandekagerne med ahornsirup, blåbær og smør.' },
-      ],
+    howToList: [
+      { howTo: 'Pisk mel og mælk sammen med en håndmixer.' },
+      { howTo: 'Tilsæt kvark og pisk æg og resten af ingredienserne i.' },
+      {
+        howTo:
+          'Fordel 4 portioner dej a ca. ½ dl på panden. Bag pandekagerne ca. 2 min. på hver side. ',
+      },
+      { howTo: 'Server pandekagerne med ahornsirup, blåbær og smør.' },
+    ],
 
-      page: 'opskrifter/amerikanske-pandekager.html',
-    },
+    page: 'opskrifter/amerikanske-pandekager.html',
+  },
 
-    {
-      id: 1,
+  {
+    id: 1,
 
-      type: 'lunch',
-      category: '/assets/lunch_icon.svg',
-      categoryAltText: 'lunch ikon',
+    type: 'lunch',
+    category: '/assets/lunch_icon.svg',
+    categoryAltText: 'lunch ikon',
 
-      image: '/assets/quesadillas-i-lag.jpg',
-      imageAltText: 'Quesadilla i lag',
+    image: '/assets/recipe-img/quesadillas-i-lag.jpg',
+    imageAltText: 'Quesadilla i lag',
 
-      title: 'Quesadillas i lag',
-      shortDescription:
-        'Quesadilla i lag er en herlig mexicansk streetfood inspiret ret, som vi ser oftere og oftere her i Danmark. Quesadilla består af totillas, der lægges sammen og gratineres med ost.',
-      description:
-        'Amerikanske pandekager passer perfekt til både brunch og dessert. Bag de små, tykke amerikanske pandekager i en almindelig pande eller i en blinispande og servér med blåbær, ahornsirup og smør. Det er både nemt og dejligt at hygge sig med at lave amerikanske pandekager. Og det går rigtig hurtigt med at få de amerikanske pandekager spist!',
+    title: 'Quesadillas i lag',
+    shortDescription:
+      'Quesadilla i lag er en herlig mexicansk streetfood inspiret ret, som vi ser oftere og oftere her i Danmark. Quesadilla består af totillas, der lægges sammen og gratineres med ost.',
+    description:
+      'Quesadilla i lag er en herlig mexicansk streetfood inspiret ret, som vi ser oftere og oftere her i Danmark. Quesadilla består af totillas, der lægges sammen og gratineres med ost. Her er quesadillaerne fyldt med peberfrugtblanding og kylling - og serveres med den helt uundværlige koriander i godt selskab. Quesadillas er en favorit fra det mexicanske streetfood-køkken, fordi det er både nemt og lækkert. Prøv vores opskrift på Quesadilllas med kylling og ost her.',
 
-      time: '30',
-      person: '4',
-      language: 'Amerikansk',
+    time: '45',
+    person: '4',
+    language: 'Mecicansk',
 
-      ingredients: [
-        {
-          list: [
-            { name: 'Hvedemel', amount: '300', type: 'g' },
-            { name: 'Mælk', amount: '3', type: 'dl' },
-            { name: 'Skyr eller græsk yoghurt', amount: '2', type: 'dl' },
-            { name: 'Æg', amount: '2', type: 'stk' },
-            { name: 'Sukker', amount: '2', type: 'spsk' },
-            { name: 'Groft salt', amount: '¼', type: 'tsk' },
-            { name: 'Bagepulver', amount: '1½', type: 'tsk' },
-            { name: 'Smør - smeltet', amount: '50', type: 'g' },
-          ],
-        },
-        {
-          listTwo: [
-            { name: 'Ahornsirup', amount: '1', type: 'dl' },
-            { name: 'Blåbær', amount: '100', type: 'g' },
-            { name: 'Smør', amount: '50', type: 'g' },
-          ],
-        },
-      ],
+    listName: 'Peberfrugtblanding',
+    listNameTwo: 'Quesadilla i lag',
 
-      howToList: [
-        { howTo: 'Pisk mel og mælk sammen med en håndmixer.' },
-        { howTo: 'Tilsæt kvark og pisk æg og resten af ingredienserne i.' },
-        {
-          howTo:
-            'Fordel 4 portioner dej a ca. ½ dl på panden. Bag pandekagerne ca. 2 min. på hver side. ',
-        },
-        { howTo: 'Server pandekagerne med ahornsirup, blåbær og smør.' },
-      ],
+    ingredients: [
+      {
+        list: [
+          { name: 'Smør', amount: '20', type: 'g' },
+          { name: 'Finthakket rødløg', amount: '1', type: 'stk' },
+          { name: 'Små knuste fed hvidløg', amount: '2', type: 'stk' },
+          { name: 'Peberfrugter', amount: '2', type: 'stk' },
+          { name: 'Tomater i mindre stykker', amount: '4', type: 'stk' },
+          { name: 'Groft salt', amount: '2', type: 'tsk' },
+          { name: 'Kyllingefilet i strimler', amount: '400', type: 'g' },
+          { name: 'Friskkværnet peber', amount: '', type: '' },
+        ],
+      },
+      {
+        listTwo: [
+          { name: 'Fuldkornstortillaer', amount: '6', type: 'stk' },
+          { name: 'revet Mozzarella', amount: '140', type: 'g' },
+          { name: 'Rødløg i tynde halve ringe', amount: '1', type: 'stk' },
+          { name: 'Friskpresset limesaft', amount: '2', type: 'spsk' },
+          { name: 'krydret tomatsauce', amount: '200', type: 'g' },
+          { name: 'Frisk koriander', amount: '25', type: 'g' },
+        ],
+      },
+    ],
 
-      page: 'opskrifter/quesadilla-i-lag.html',
-    },
+    howToList: [
+      {
+        howTo:
+          'Lad smørret smelte i en stor pande ved kraftig varme, men uden at det bruner.',
+      },
+      { howTo: 'Svits løg og hvidløg i ca. 1 min.' },
+      {
+        howTo:
+          'Tilsæt peberfrugter, tomater, salt og peber og svits i yderligere ca. 4 min.',
+      },
+      { howTo: 'Hæld peberfrugtblandingen i en skål og tør panden af.' },
+      {
+        howTo:
+          '<strong>Kylling</strong> <br>Lad smørret blive gyldent i panden og steg kyllingestrimlerne i ca. 3 min., drys med salt og peber.',
+      },
+      {
+        howTo:
+          '<strong>Quesadillas i lag</strong> <br>Læg tortillaerne sammen 3 og 3 således; Læg 1 tortilla på en plade med bagepapir og fordel herpå ¼ af peberfrugtblandingen, ¼ af kyllingekødet og lidt af osten.',
+      },
+      { howTo: 'Gentag og afslut med 1 tortilla og lidt ost på toppen.' },
+      {
+        howTo:
+          'Gør det samme med de sidste 3 tortillaer på en anden bageplade.',
+      },
+      {
+        howTo: 'Gratiner dem indtil osten er smeltet og gylden.',
+      },
+      {
+        howTo:
+          '<strong>Servering af quesadillas i lag</strong><br> Læg imens agurkerne på et lille fladt fad.',
+      },
+      {
+        howTo: 'Bland rødløg og limesaft sammen og fordel det over agurkerne.',
+      },
+      {
+        howTo:
+          'Del de bagte quesadillas og server dem sammen med agurker og salsa.',
+      },
+      {
+        howTo: 'Top det hele med friske koriander.',
+      },
+      {
+        howTo: '<strong></strong> Gratinering Ca. 5 min. ved 225° - varmluft.',
+      },
+    ],
 
-    {
-      id: 2,
+    page: 'opskrifter/quesadilla-i-lag.html',
+  },
 
-      type: 'dinner',
-      category: '/assets/dinner_icon.svg',
-      categoryAltText: 'dinner ikon',
+  {
+    id: 2,
 
-      image: '/assets/tomatsuppe-med-peberfrugt-og-sprod-gremolata.jpg',
-      imageAltText: 'tomatsuppe med peberfrugt og sprød gremolata',
+    type: 'dinner',
+    category: '/assets/dinner_icon.svg',
+    categoryAltText: 'dinner ikon',
 
-      title: 'Tomatsuppe med sprød gremolata',
-      description:
-        'En super hurtig vegetarisk tomatsuppe med flotte farver. Tomatsuppen er fyldt med grøntsager. Som ekstra pift er den toppet op med en sprød gremolata.',
+    image: '/assets/tomatsuppe-med-peberfrugt-og-sprod-gremolata.jpg',
+    imageAltText: 'tomatsuppe med peberfrugt og sprød gremolata',
 
-      time: '25',
-      person: '4',
-      language: 'Spansk',
+    title: 'Tomatsuppe med sprød gremolata',
+    description:
+      'En super hurtig vegetarisk tomatsuppe med flotte farver. Tomatsuppen er fyldt med grøntsager. Som ekstra pift er den toppet op med en sprød gremolata.',
 
-      page: 'opskrifter/tomatsuppe-med-sprød-gremolata.html',
-    },
+    time: '25',
+    person: '4',
+    language: 'Spansk',
 
-    {
-      id: 3,
+    page: 'opskrifter/tomatsuppe-med-sprød-gremolata.html',
+  },
 
-      type: 'dessert',
-      category: '/assets/dessert_icon.svg',
-      categoryAltText: 'dessert ikon',
+  {
+    id: 3,
 
-      image: '/assets/creme-brulee.jpg',
-      imageAltText: 'creme brulee',
+    type: 'dessert',
+    category: '/assets/dessert_icon.svg',
+    categoryAltText: 'dessert ikon',
 
-      title: 'Creme brulee',
-      description:
-        'Creme brulee er en af de klassiske franske desserter, der består af fløde, vanilje, æggeblommer og sukker afsluttet med et tyndt låg af sprødt brændt sukker.',
+    image: '/assets/creme-brulee.jpg',
+    imageAltText: 'creme brulee',
 
-      time: '25',
-      person: '4',
-      language: 'Fransk',
+    title: 'Creme brulee',
+    description:
+      'Creme brulee er en af de klassiske franske desserter, der består af fløde, vanilje, æggeblommer og sukker afsluttet med et tyndt låg af sprødt brændt sukker.',
 
-      page: 'opskrifter/creme-brulee.html',
-    },
+    time: '25',
+    person: '4',
+    language: 'Fransk',
 
-    {
-      id: 4,
+    page: 'opskrifter/creme-brulee.html',
+  },
 
-      type: 'dinner',
-      category: '/assets/dinner_icon.svg',
-      categoryAltText: 'dinner ikon',
+  {
+    id: 4,
 
-      image: '/assets/wok-med-kylling-nudler-og-grontsager.jpg',
-      imageAltText: 'Wok med kylling, nudler og grøntsager',
+    type: 'dinner',
+    category: '/assets/dinner_icon.svg',
+    categoryAltText: 'dinner ikon',
 
-      title: 'Wok med kylling, nudler og grøntsager',
-      description:
-        'Wok med kylling, nudler og grøntsager - en dejlig asiatisk inspireret wokret med masser af grønt og sprød topping af ristede cashewnødder og friske forårsløg. ',
+    image: '/assets/wok-med-kylling-nudler-og-grontsager.jpg',
+    imageAltText: 'Wok med kylling, nudler og grøntsager',
 
-      time: '30',
-      person: '4',
-      language: 'Japansk',
+    title: 'Wok med kylling, nudler og grøntsager',
+    description:
+      'Wok med kylling, nudler og grøntsager - en dejlig asiatisk inspireret wokret med masser af grønt og sprød topping af ristede cashewnødder og friske forårsløg. ',
 
-      page: 'opskrifter/wok-med-kylling-nudler-og-grøntsager.html',
-    },
+    time: '30',
+    person: '4',
+    language: 'Japansk',
 
-    {
-      id: 5,
+    page: 'opskrifter/wok-med-kylling-nudler-og-grøntsager.html',
+  },
 
-      type: 'lunch',
-      category: '/assets/lunch_icon.svg',
-      categoryAltText: 'lunch ikon',
+  {
+    id: 5,
 
-      image: '/assets/mozzarella-sticks.jpg',
-      imageAltText: 'Mozzarella sticks',
+    type: 'lunch',
+    category: '/assets/lunch_icon.svg',
+    categoryAltText: 'lunch ikon',
 
-      title: 'Mozzarella sticks',
-      description:
-        'Cheesesticks eller mozzarella sticks er fastfood eller street food, når det er rigtig fedt eller fedtet... Her bliver mozarella sticks serveret i en ny udgave med mere smag.',
+    image: '/assets/mozzarella-sticks.jpg',
+    imageAltText: 'Mozzarella sticks',
 
-      time: '30',
-      person: '4',
-      language: 'Fransk',
+    title: 'Mozzarella sticks',
+    description:
+      'Cheesesticks eller mozzarella sticks er fastfood eller street food, når det er rigtig fedt eller fedtet... Her bliver mozarella sticks serveret i en ny udgave med mere smag.',
 
-      page: 'opskrifter/mozzarella-sticks.html',
-    },
+    time: '30',
+    person: '4',
+    language: 'Fransk',
 
-    {
-      id: 6,
+    page: 'opskrifter/mozzarella-sticks.html',
+  },
 
-      type: 'breakfast',
-      category: '/assets/breakfast_icon.svg',
-      categoryAltText: 'breakfast ikon',
+  {
+    id: 6,
 
-      image: '/assets/belgiske-vafler.jpg',
-      imageAltText: 'Belgiske vafler',
+    type: 'breakfast',
+    category: '/assets/breakfast_icon.svg',
+    categoryAltText: 'breakfast ikon',
 
-      title: 'Belgiske vafler',
-      description:
-        'Belgiske vafler er en rigtig dejlig hyggespise. Server hjemmelavede belgiske vafler efter en kølig gåtur, til weekendens morgenmad eller til brunch. Vaflerne er sprøde udenpå og bløde indeni.',
+    image: '/assets/belgiske-vafler.jpg',
+    imageAltText: 'Belgiske vafler',
 
-      time: '30',
-      person: '3',
-      language: 'Belgiske',
+    title: 'Belgiske vafler',
+    description:
+      'Belgiske vafler er en rigtig dejlig hyggespise. Server hjemmelavede belgiske vafler efter en kølig gåtur, til weekendens morgenmad eller til brunch. Vaflerne er sprøde udenpå og bløde indeni.',
 
-      page: 'opskrifter/belgiske-vafler.html',
-    },
+    time: '30',
+    person: '3',
+    language: 'Belgiske',
 
-    {
-      id: 7,
+    page: 'opskrifter/belgiske-vafler.html',
+  },
 
-      type: 'dessert',
-      category: '/assets/dessert_icon.svg',
-      categoryAltText: 'dessert ikon',
+  {
+    id: 7,
 
-      image: '/assets/muffins-med-chokoladestykker.jpg',
-      imageAltText: 'Muffins med chokoladestykker',
+    type: 'dessert',
+    category: '/assets/dessert_icon.svg',
+    categoryAltText: 'dessert ikon',
 
-      title: 'Muffins med chokoladestykker',
-      description:
-        'Muffins er betegnelsen for en meget populær kage, som er bagt i en lille, rund portionsform - og der findes et utal af varianter.',
+    image: '/assets/muffins-med-chokoladestykker.jpg',
+    imageAltText: 'Muffins med chokoladestykker',
 
-      time: '45',
-      person: '4',
-      language: 'Amerikansk',
+    title: 'Muffins med chokoladestykker',
+    description:
+      'Muffins er betegnelsen for en meget populær kage, som er bagt i en lille, rund portionsform - og der findes et utal af varianter.',
 
-      page: 'opskrifter/muffins-med-chokoladestykker.html',
-    },
+    time: '45',
+    person: '4',
+    language: 'Amerikansk',
 
-    {
-      id: 8,
+    page: 'opskrifter/muffins-med-chokoladestykker.html',
+  },
 
-      type: 'dinner',
-      category: '/assets/dinner_icon.svg',
-      categoryAltText: 'dinner ikon',
+  {
+    id: 8,
 
-      image: '/assets/spaghetti-carbonara.jpg',
-      imageAltText: 'Spaghetti Carbonara',
+    type: 'dinner',
+    category: '/assets/dinner_icon.svg',
+    categoryAltText: 'dinner ikon',
 
-      title: 'Spaghetti Carbonara',
-      description:
-        'Spaghetti Carbonara - næsten som at sidde på en italiensk restaurant. Vi foreslår en tomatsalat til Spaghetti Cabonara. Det ville de nok ikke gøre i Italien, de ville spise salaten bagefter.',
+    image: '/assets/spaghetti-carbonara.jpg',
+    imageAltText: 'Spaghetti Carbonara',
 
-      time: '45',
-      person: '4',
-      language: 'Italiensk ',
+    title: 'Spaghetti Carbonara',
+    description:
+      'Spaghetti Carbonara - næsten som at sidde på en italiensk restaurant. Vi foreslår en tomatsalat til Spaghetti Cabonara. Det ville de nok ikke gøre i Italien, de ville spise salaten bagefter.',
 
-      page: 'opskrifter/spaghetti-carbonara.html',
-    },
-  ];
+    time: '45',
+    person: '4',
+    language: 'Italiensk ',
 
-  export default recipeData;
+    page: 'opskrifter/spaghetti-carbonara.html',
+  },
+];
+
+export default recipeData;
