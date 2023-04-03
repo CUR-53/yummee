567d14928e499c751655663b42deb5d2737d5ca0
CUR53 <114309130+CUR-53@users.noreply.github.com>
2023-04-03 22:07:18 +0200

    search bar
    a holde new search bar

--- scripts/suggestions.js
+++ scripts/suggestions.js
@@ -1,68 +1,31 @@
-let suggestions = [
-  'amerikanske pandekager',
-  'quesadilla i lag',
-  'tomatsuppe med peberfrugt og sprød gremolata',
-  'creme brulee',
-  'wok med kylling nudler og grøntsager',
-  'mozzarella sticks',
-  'belgiske vafler',
-  'muffins med chokoladestykker',
-  'spaghetti carbonara',
-];
+import recipeData from './recipes.js';
 
-const searchWrapper = document.querySelector('.search_input');
-const inputBox = searchWrapper.querySelector('input');
-const suggBox = searchWrapper.querySelector('.autocom_box');
-const icon = searchWrapper.querySelector('.search_icon');
-let linkTag = searchWrapper.querySelector('a');
-let webLink;
+const searchInput = document.querySelector('input[name="recipe_search"]');
+const autocomBox = document.querySelector('.autocom_box');
 
-// if user press any key and release
-inputBox.onkeyup = (e) => {
-  let userData = e.target.value; //user enetered data
-  let emptyArray = [];
-  if (userData) {
-    icon.onclick = () => {
-      webLink = `opskrifter/${encodeURIComponent(selectData)}.html`;
-      linkTag.setAttribute('href', webLink);
-      linkTag.click();
-    };
-    emptyArray = suggestions.filter((data) => {
-      //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-    });
-    emptyArray = emptyArray.map((data) => {
-      // passing return data inside li tag
-      return (data = `<li onclick="location.href='opskrifter/${data.replace(
-        / /g,
-        '-'
-      )}.html';">${data}</li>`);
-    });
-    searchWrapper.classList.add('active'); //show autocomplete box
-    showSuggestions(emptyArray);
-  } else {
-    searchWrapper.classList.remove('active'); //hide autocomplete box
+searchInput.addEventListener('keyup', function (event) {
+  const inputText = searchInput.value.toLowerCase();
+  const suggestions = recipeData.filter(function (recipe) {
+    return recipe.title.toLowerCase().includes(inputText);
+  });
+
+  autocomBox.innerHTML = '';
+  suggestions.forEach(function (suggestedRecipe) {
+    const suggestionLink = document.createElement('a');
+    suggestionLink.href = `opskrifter/template.html?id=${suggestedRecipe.id}`;
+    suggestionLink.innerText = suggestedRecipe.title;
+    autocomBox.appendChild(suggestionLink);
+  });
+
+  // Select the first suggestion when pressing enter
+  if (event.keyCode === 13 && suggestions.length > 0) {
+    window.location.href = `opskrifter/template.html?id=${suggestions[0].id}`;
   }
-};
+});
 
-function select(element) {
-  let selectData = element.textContent;
-  inputBox.value = selectData;
-  icon.onclick = () => {
-    webLink = `recipes/${encodeURIComponent(selectData)}.html`;
-    linkTag.setAttribute('href', webLink);
-    linkTag.click();
-  };
-  searchWrapper.classList.remove('active');
-}
-
-function showSuggestions(list) {
-  let listData;
-  if (!list.length) {
-    userValue = inputBox.value;
-    listData = `<li>${userValue}</li>`;
-  } else {
-    listData = list.join('');
+// Close the suggestions box when clicking outside of it
+document.addEventListener('click', function (event) {
+  if (!autocomBox.contains(event.target)) {
+    autocomBox.innerHTML = '';
   }
-  suggBox.innerHTML = listData;
-}
+});
